dc13920aaa42e9e13c6b22b27033091281f94b78
Thiam <khaly_sileye.thiam@edu.univ-fcomte.fr>
2025-04-06 12:28:28 +0200

    Fix all page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "GesVillage - Gestion intelligente de l'eau",
+  title: "GesVillage - gestion de l'eau dans les zones rurales",
   description: "Plateforme de gestion des forages d'eau pour les collectivités locales, village et privé",
 };
 
